2d57eb99f4ce99001f270aaa78a3c8c705e2fe66
wangyaodream <wangyao.dream@hotmail.com>
2024-12-06 16:05:28 +0800

    json encoded

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "status: available")
-    fmt.Fprintf(w, "environment: %s\n", app.config.env)
-    fmt.Fprintf(w, "version: %s\n", version)
+    data := map[string]string{
+        "status": "available",
+        "environment": app.config.env,
+        "version": version,
+    }
+
+    // 使用自定义模块helper中的writeJSON来转换数据
+    err := app.writeJSON(w, http.StatusOK, data, nil)
+
+    if err != nil {
+        app.logger.Print(err)
+        http.Error(w, "The server encoutered a problem and could not process your request", http.StatusInternalServerError)
+    }
 }
