84674c6085659ce29dd8a2b00ff7815cca176219
wangyao <wangyao.dream@hotmail.com>
2024-12-11 21:53:18 +0800

    Implement update and delete

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -39,20 +39,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-    // 保存到数据库
-    err = app.models.Movies.Insert(movie)
-    if err != nil {
-        app.serverErrorResponse(w, r, err)
-        return
-    }
+	// 保存到数据库
+	err = app.models.Movies.Insert(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 
-    headers := make(http.Header)
-    headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
 
-    err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
-    if err != nil {
-        app.serverErrorResponse(w, r, err)
-    }
+	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -64,21 +64,105 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-    // 获取id对应的movie
-    movie, err := app.models.Movies.Get(id)
-    if err != nil {
-        switch {
-        case errors.Is(err, data.ErrRecordNotFound):
-            app.notFoundResponse(w, r)
-        default:
-            app.serverErrorResponse(w, r, err)
-        }
-        return
-    }
-    
-    // 返回movie
-    err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
-    if err != nil {
-        app.serverErrorResponse(w, r, err)
-    }
+	// 获取id对应的movie
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// 返回movie
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	// 获取id对应的movie
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie.Title = input.Title
+	movie.Year = input.Year
+	movie.Runtime = input.Runtime
+	movie.Genres = input.Genres
+
+	// 验证
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// 更新到数据库
+	err = app.models.Movies.Update(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// 返回movie到客户端
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
+
+func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	err = app.models.Movies.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "movie successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -20,68 +20,91 @@ type Movie struct {
 }
 
 type MovieModel struct {
-    DB *sql.DB
+	DB *sql.DB
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
-    query := `
+	query := `
         INSERT INTO movies (title, year, runtime, genres)
         VALUES ($1, $2, $3, $4)
         RETURNING id, created_at, version
     `
-    args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
 
-    return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
-    if id < 1 {
-        return nil, ErrRecordNotFound
-    }
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
 
-    query := `
+	query := `
         SELECT id, created_at, title, year, runtime, genres, version
         FROM movies
         WHERE id = $1
     `
-    var movie Movie
-    err := m.DB.QueryRow(query, id).Scan(
-        &movie.ID,
-        &movie.CreatedAt,
-        &movie.Title,
-        &movie.Year,
-        &movie.Runtime,
-        pq.Array(&movie.Genres),
-        &movie.Version,
-    )
+	var movie Movie
+	err := m.DB.QueryRow(query, id).Scan(
+		&movie.ID,
+		&movie.CreatedAt,
+		&movie.Title,
+		&movie.Year,
+		&movie.Runtime,
+		pq.Array(&movie.Genres),
+		&movie.Version,
+	)
 
-    if err != nil {
-        switch {
-        case errors.Is(err, sql.ErrNoRows):
-            return nil, ErrRecordNotFound
-        default:
-            return nil, err
-        }
-    }
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
 
-    return &movie, nil
+	return &movie, nil
 }
 
 func (m MovieModel) Update(movie *Movie) error {
-    query := `
+	query := `
         UPDATE movies
         SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
         WHERE id = $5
         RETURNING version
     `
 
-    args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID}
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID}
 
-    return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
 
 func (m MovieModel) Delete(id int64) error {
-    return nil
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `
+        DELETE FROM movies
+        WHERE id = $1
+    `
+
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
+	return nil
 }
 
 func ValidateMovie(v *validator.Validator, movie *Movie) {
