91204fe70ed53ee000383e3b9c2974fec17e0f79
wangyaodream <wangyao.dream@hotmail.com>
2024-12-13 17:36:53 +0800

    implementing optimistic locking

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -39,3 +39,8 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
+
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+    message := "ubable to update the record due to an edit conflict, please try again"
+    app.errorResponse(w, r, http.StatusConflict, message)
+}
