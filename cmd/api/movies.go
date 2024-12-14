94a6f5ff8358e92a82205b4551431beedf12ee3e
wangyao <wangyao.dream@hotmail.com>
2024-12-14 17:47:18 +0800

    listMovieHandler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -183,3 +183,34 @@ func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request) {
+    var input struct {
+        Title string
+        Genres []string
+        Page int
+        PageSize int
+        Sort string
+    }
+
+    // 创建一个新的验证器
+    v := validator.New()
+
+    // 解析查询参数
+    qs := r.URL.Query()
+
+    input.Title = app.readString(qs, "title", "")
+    input.Genres = app.readCSV(qs, "genres", []string{})
+    input.Page = app.readInt(qs, "page", 1, v)
+    input.PageSize = app.readInt(qs, "page_size", 20, v)
+    input.Sort = app.readString(qs, "sort", "id")
+
+    // 如果有错误，返回错误信息
+    if !v.Valid() {
+        app.failedValidationResponse(w, r, v.Errors)
+        return
+    }
+
+    fmt.Fprintf(w, "%v\n", input)
+
+}
