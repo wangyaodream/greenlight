912b1aa9dc32173262c3864fc538611ff3f740cf
wangyaodream <wangyao.dream@hotmail.com>
2024-12-20 16:53:56 +0800

    use middleware recoverPainc

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	// 初始化httprouter实例
 	router := httprouter.New()
 
@@ -22,6 +22,8 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
-	return router
+    
+
+	return app.recoverPanic(router)
 
 }
