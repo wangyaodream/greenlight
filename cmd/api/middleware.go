8a62ed763a539f619f4405e3d3e2f5a6c7c404cc
wangyao <wangyao.dream@hotmail.com>
2024-12-21 23:41:12 +0800

    use ip rateLimit

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,34 +2,52 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
 
 	"golang.org/x/time/rate"
 )
 
-
 func (app *application) recoverPanic(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-        defer func() {
-            if err := recover(); err != nil {
-                w.Header().Set("Connection", "close")
-                app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
-            }
-        }()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
 
-        next.ServeHTTP(w, r)
-    })
+		next.ServeHTTP(w, r)
+	})
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-    limiter := rate.NewLimiter(2, 4)
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*rate.Limiter)
+	)
 
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-        if !limiter.Allow() {
-            app.rateLimitExceededResponse(w, r)
-            return
-        }
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
 
-        next.ServeHTTP(w, r)
-    })
+		mu.Lock()
+		if _, found := clients[ip]; !found {
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
+		if !clients[ip].Allow() {
+			mu.Unlock()
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+
+		mu.Unlock()
+
+		next.ServeHTTP(w, r)
+	})
 }
