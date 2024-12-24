345b772e92fe783ab5edc56e88d9746f100aac1a
wangyaodream <wangyao.dream@hotmail.com>
2024-12-24 17:41:52 +0800

    release serve from main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -78,21 +78,10 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", app.config.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-
-	err = srv.ListenAndServe()
-	logger.PrintFatal(err, nil)
+    err = app.serve()
+    if err != nil {
+        logger.PrintFatal(err, nil)
+    }
 
 }
 
