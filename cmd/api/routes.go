1d4c441f27017121163900874b0ea0cbc489545d
wangyao <wangyao.dream@hotmail.com>
2025-01-13 17:40:29 +0800

    add metrics

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -29,6 +30,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 	// create authentication token
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
+    // 注册一个新的GET /debuge/vars路由
+    router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
     // 添加enbaleCORS中间件
 	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
