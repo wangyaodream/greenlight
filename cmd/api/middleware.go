b2b055b9dcd02887ec884fd90c9b1e487dc673da
wangyao <wangyao.dream@hotmail.com>
2025-01-15 23:36:21 +0800

    add response status

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -14,6 +15,40 @@ import (
 	"github.com/wangyaodream/greenlight/internal/validator"
 	"golang.org/x/time/rate"
 )
+
+type metricsResponseWriter struct {
+    wrapped http.ResponseWriter
+    statusCode int
+    headerWritten bool
+}
+
+func (mw *metricsResponseWriter) Header() http.Header {
+    return mw.wrapped.Header()
+}
+
+func (mw *metricsResponseWriter) WriteHeader(statusCode int) {
+    mw.wrapped.WriteHeader(statusCode)
+
+    if !mw.headerWritten {
+        mw.statusCode = statusCode
+        mw.headerWritten = true
+    }
+}
+
+func (mw *metricsResponseWriter) Write(b []byte) (int, error) {
+    if !mw.headerWritten {
+        mw.WriteHeader(http.StatusOK)
+        mw.headerWritten = true
+    }
+
+    return mw.wrapped.Write(b)
+}
+
+func (mw *metricsResponseWriter) Unwrap() http.ResponseWriter {
+    return mw.wrapped
+}
+
+
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -238,15 +273,25 @@ func (app *application) metrics(next http.Handler) http.Handler {
         totalRequestsReceived = expvar.NewInt("total_requests_received")
         totalResponsesSent = expvar.NewInt("total_responses_sent")
         totalProcessingTimeMicroseconds = expvar.NewInt("total_processing_time_microseconds")
+
+        totalResponseSendByStatus = expvar.NewMap("total_responses_sent_by_status")
     )
 
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         start := time.Now()
 
         totalRequestsReceived.Add(1)
-        next.ServeHTTP(w, r)
+
+        // 创建一个新的 metricsResponseWriter
+        mw := &metricsResponseWriter{wrapped: w}
+
+
+        next.ServeHTTP(mw, r)
 
         totalResponsesSent.Add(1)
+
+        // 将响应状态码添加到totalResponseSendByStatus
+        totalResponseSendByStatus.Add(strconv.Itoa(mw.statusCode), 1)
 
         duration := time.Since(start).Microseconds()
         totalProcessingTimeMicroseconds.Add(duration)
